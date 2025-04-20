134591db62c7a07c0df4e7373b97c61696cbfba9
gavogt <gvogt@asu.edu>
2025-04-19 20:47:20 -0700

    Added an fgets in GetUserChoice to handle the user input

--- XenoCyborgCommandCenter/xeno_cyborg.c
+++ XenoCyborgCommandCenter/xeno_cyborg.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "xeno_cyborg.h"
 
 GetUserChoice() {
+	// Variable declarations
+	char buffer[50];
+	int choice;
 
 	puts("*** MENU OPTIONS ***");
 	puts("1) Add new cyborg");
@@ -11,4 +15,22 @@ GetUserChoice() {
 	puts("5) Save & Exit");
 	puts("6) Exit without saving");
 	printf("Enter your choice: ");
+
+	if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+		buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline character		
+		choice = atoi(buffer); // Convert string to integer
+
+		if (choice < 1 || choice > 6) {
+			fprintf(stderr, "Invalid choice. Please enter a number between 1 and 6.\n");
+			EXIT_FAILURE;
+		}
+		else {
+			printf("You selected option %d\n", choice);
+			return choice;
+		}
+	}
+	else {
+		fprintf(stderr, "Error reading input\n");
+		EXIT_FAILURE;
+	}
 }
