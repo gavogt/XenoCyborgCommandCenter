16485327645af980d28cbcd31fab28f0e498e858
Gabriel <gvogt@asu.edu>
2025-04-20 15:11:45 -0700

    Added a while loop for MenuSwitch and GetUserChoice

--- XenoCyborgCommandCenter/main.c
+++ XenoCyborgCommandCenter/main.c
@@ -16,92 +16,96 @@ int main(int argc, char* argv[]) {
 
 	int choice = 0;
 
-	choice = GetUserChoice();
+	while (1) {
+
+		choice = GetUserChoice();
 
 
-	// Initialize variables
-	AlienCyborg* cyborgs = NULL;
-	int count = 0;
-	size_t capacity = 0;
+		// Initialize variables
+		AlienCyborg* cyborgs = NULL;
+		int count = 0;
+		size_t capacity = 0;
 
-	if (cyborgs == NULL) {
-		capacity = 4; // Initial capacity
-		cyborgs = malloc(capacity * sizeof * cyborgs); // Allocate memory for cyborgs
 		if (cyborgs == NULL) {
-			perror("Failed to allocate memory");
-			exit(EXIT_FAILURE);
-		}
-		else {
-			printf("Memory allocated successfully\n");
-		}
-	}
-	// If no cyborg was created will return 0
-	AlienCyborg newCyborg = MenuSwitch(choice);
-
-	// If newCyborg.id is not 0, it means a cyborg was created
-	if (newCyborg.id != 0) {
-		if (count == capacity) {
-			capacity *= 2; // Double the capacity if at capacity
-			// Prevent memory leak by reallocating memory so no loss of memoryblock for cyborgs into tmp pointer
-			AlienCyborg* tmp = realloc(cyborgs, capacity * sizeof * cyborgs);
-			if (!tmp) {
-				perror("Failed to reallocate memory");
-				free(cyborgs); // Free previously allocated memory
+			capacity = 4; // Initial capacity
+			cyborgs = malloc(capacity * sizeof * cyborgs); // Allocate memory for cyborgs
+			if (cyborgs == NULL) {
+				perror("Failed to allocate memory");
 				exit(EXIT_FAILURE);
 			}
-
-			cyborgs = tmp; // Update pointer to pointer to the newly allocated memory
-
-		}
-		cyborgs[count++] = newCyborg; // Add new cyborg to the array
-
-		// Let user know success
-		printf("Cyborg added successfully. Total cyborgs: %d\n", count);
-	}
-
-	// Get filename from argv
-	if (argc > 1) {
-		const char* fname = argv[1];
-
-		// prompt for mode required
-		char modeStr[LINE_MAX];
-		int mode = 0;
-
-		printf("Load '%s' as 1) binary or 2) text :", fname);
-
-		// Read user's choice
-		if (read_line(modeStr, sizeof modeStr))
-			mode = (int)strtol(modeStr, NULL, 10); // Convert string to long then cast as integer
-
-		if (mode != 1 && mode != 2) {
-			puts("invalid choice");
-			mode = 0;
-		}
-
-		// Open file in the specified mode
-		if (mode) {
-			// Open file in binary mode if mode is 1 and read file if mode is not 1
-			FILE* file = fopen(fname, mode == 1 ? "rb" : "r");
-			// Check if there is a file
-			if (!file) {
-				perror("Failed to open file");
-				exit(EXIT_FAILURE);
+			else {
+				printf("Memory allocated successfully\n");
 			}
-			else if (mode == 1) {
-				// Read 1 item size of count from file store it in count
-				if (fread(&count, sizeof(count), 1, file) == 1 &&
-					count <= MAX_CYBORGS &&
-					// Load all cyborgs into memory
-					(fread(cyborgs, sizeof * cyborgs, count, file) == (size_t)count)) {
-					// Successfully read cyborgs from binary file
-					printf("Read %d cyborgs from binary file\n", count);
-				}
-				else {
-					perror("Failed to read cyborgs from binary file");
+		}
+		// If no cyborg was created will return 0
+		AlienCyborg newCyborg = MenuSwitch(choice);
+
+		// If newCyborg.id is not 0, it means a cyborg was created
+		if (newCyborg.id != 0) {
+			if (count == capacity) {
+				capacity *= 2; // Double the capacity if at capacity
+				// Prevent memory leak by reallocating memory so no loss of memoryblock for cyborgs into tmp pointer
+				AlienCyborg* tmp = realloc(cyborgs, capacity * sizeof * cyborgs);
+				if (!tmp) {
+					perror("Failed to reallocate memory");
+					free(cyborgs); // Free previously allocated memory
 					exit(EXIT_FAILURE);
 				}
 
-				fclose(file);
+				cyborgs = tmp; // Update pointer to pointer to the newly allocated memory
+
+			}
+			cyborgs[count++] = newCyborg; // Add new cyborg to the array
+
+			// Let user know success
+			printf("Cyborg added successfully. Total cyborgs: %d\n", count);
+		}
+
+
+		// Get filename from argv
+		if (argc > 1) {
+			const char* fname = argv[1];
+
+			// prompt for mode required
+			char modeStr[LINE_MAX];
+			int mode = 0;
+
+			printf("Load '%s' as 1) binary or 2) text :", fname);
+
+			// Read user's choice
+			if (read_line(modeStr, sizeof modeStr))
+				mode = (int)strtol(modeStr, NULL, 10); // Convert string to long then cast as integer
+
+			if (mode != 1 && mode != 2) {
+				puts("invalid choice");
+				mode = 0;
+			}
+
+			// Open file in the specified mode
+			if (mode) {
+				// Open file in binary mode if mode is 1 and read file if mode is not 1
+				FILE* file = fopen(fname, mode == 1 ? "rb" : "r");
+				// Check if there is a file
+				if (!file) {
+					perror("Failed to open file");
+					exit(EXIT_FAILURE);
+				}
+				else if (mode == 1) {
+					// Read 1 item size of count from file store it in count
+					if (fread(&count, sizeof(count), 1, file) == 1 &&
+						count <= MAX_CYBORGS &&
+						// Load all cyborgs into memory
+						(fread(cyborgs, sizeof * cyborgs, count, file) == (size_t)count)) {
+						// Successfully read cyborgs from binary file
+						printf("Read %d cyborgs from binary file\n", count);
+					}
+					else {
+						perror("Failed to read cyborgs from binary file");
+						exit(EXIT_FAILURE);
+					}
+
+					fclose(file);
+				}
 			}
 		}
 	}
